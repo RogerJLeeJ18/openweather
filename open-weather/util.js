cb1608f247b0630c5ae02c1d7395295a85d1fed7
Joey Delahoussaye <jldela@gmail.com>
2018-11-29 21:49:35 -0600

    Update util.js

--- open-weather/util.js
+++ open-weather/util.js
@@ -77,8 +77,4 @@ const getAvgHumidity = (list) => {
   return Math.round(sum / list.length);
 };
 
-module.exports = {
-  apiKey: 'dcb1aac1b716c4eb71246c10bfb700ec'
-};
-
 module.exports = { cleanupForecastData, cleanupCurrentWeather };
